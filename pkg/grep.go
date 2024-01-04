4813459142c58e22e606704bdbc4c2f8145f5ea0
justsushant <sushantcod3s@gmail.com>
2024-01-04 21:21:39 +0530

    dead end

--- pkg/grep.go
+++ pkg/grep.go
@@ -13,27 +13,18 @@ var (
 	ErrIsDirectory = errors.New("is a directory")
 )
 
-func SearchString(fSys fs.FS, path string, stdin io.Reader, keyword string, ignoreCase bool) ([]string, error) {
-	var scanner *bufio.Scanner
-
-	if path != "" {
-		// check for validity
-		err := isValid(fSys, path)
-		if err != nil {
-			return nil, err
-		}
-
-		// open file
-		file, err := fSys.Open(path)
-		if err != nil {
-			return nil, err
-		}
-		defer file.Close()
-
-		scanner = bufio.NewScanner(file)
-	} else {
-		scanner = bufio.NewScanner(stdin)
+func SearchString(fSys fs.FS, path string, stdin io.Reader, keyword string, ignoreCase bool, searchDir bool) ([]string, error) {
+	err := isValid(fSys, path, searchDir)
+	if err != nil {
+		return nil, err
 	}
+
+	scanner, err, cleanup := getScanner(fSys, path, stdin)
+	if err != nil {
+		return nil, err
+	}
+	defer cleanup()
+	
 
 	result, err := search(scanner, keyword, ignoreCase)
 	if err != nil {
@@ -43,7 +34,11 @@ func SearchString(fSys fs.FS, path string, stdin io.Reader, keyword string, igno
 	return result, nil	
 }
 
-func isValid(fSys fs.FS, path string) error {
+func isValid(fSys fs.FS, path string, searchDir bool) error {
+	if path == "" {
+		return nil
+	}
+
 	fileInfo, err := fs.Stat(fSys, path)
 	if err != nil {
 		if errors.Is(err, fs.ErrNotExist) {
@@ -52,18 +47,34 @@ func isValid(fSys fs.FS, path string) error {
 		return fmt.Errorf("%s: %w", path, err)
 	}
 
-	// checks for directory
-	if fileInfo.IsDir() {
-		return fmt.Errorf("%s: %w", path, ErrIsDirectory)
-	}
+	if !searchDir {
+		// checks for directory
+		if fileInfo.IsDir() {
+			return fmt.Errorf("%s: %w", path, ErrIsDirectory)
+		}
 
-	// checks for permissions
-	// looks hacky, might have to change later
-	if fileInfo.Mode().Perm()&400 == 0 {
-		return fmt.Errorf("%s: %w", path, fs.ErrPermission)
+		// checks for permissions
+		// looks hacky, might have to change later
+		if fileInfo.Mode().Perm()&400 == 0 {
+			return fmt.Errorf("%s: %w", path, fs.ErrPermission)
+		}
 	}
 
 	return nil
+}
+
+func getScanner(fSys fs.FS, path string, stdin io.Reader) (*bufio.Scanner, error, func()) {
+	if path != "" {
+		// open file
+		file, err := fSys.Open(path)
+		if err != nil {
+			return nil, err, func(){}
+		}
+
+		return bufio.NewScanner(file), nil, func() {file.Close()}
+	}
+
+	return bufio.NewScanner(stdin), nil, func(){}
 }
 
 func search(scanner *bufio.Scanner, keyword string, ignoreCase bool) ([]string, error) {
--- pkg/grep_test.go
+++ pkg/grep_test.go
@@ -14,7 +14,7 @@ func TestSearchString(t *testing.T) {
 	file2Name := "file2.txt"
 	dir1Name := "testDir"
 	file4Name := "non-existent.txt"
-	dir2Name := "tests"
+	// dir2Name := "tests"
 
 	file1Data := []byte("this\nis\na\nfile\nIs")
 	file2Data := []byte{}
@@ -23,12 +23,17 @@ func TestSearchString(t *testing.T) {
 		file1Name: {Data: file1Data, Mode: 0755},
 		file2Name: {Data: file2Data, Mode: 0000},
 		dir1Name: {Data: nil, Mode: fs.ModeDir},
+		
+		// "tests/test1.txt": {Data: []byte("Dummy Line\nthis is a test file\none can test a program by running test cases"), Mode: 0755},
+		// "tests/filexyz.txt" : {Data: []byte("no matches here"), Mode: 0755},
+    	// "tests/inner/test1.txt" : {Data: []byte("dummy file"), Mode: 0755},
+    	// "tests/inner/test2.txt" : {Data: []byte("this file contains a test line"), Mode: 0755},
 	}
 
-	testFS["tests/test1.txt"] = &fstest.MapFile{Data: []byte("Dummy Line\nthis is a test file\none can test a program by running test cases"), Mode: 0755}
-    testFS["tests/filexyz.txt"] = &fstest.MapFile{Data: []byte("no matches here"), Mode: 0755}
-    testFS["tests/inner/test1.txt"] = &fstest.MapFile{Data: []byte("dummy file"), Mode: 0755}
-    testFS["tests/inner/test2.txt"] = &fstest.MapFile{Data: []byte("this file contains a test line"), Mode: 0755}
+	// testFS["tests/test1.txt"] = &fstest.MapFile{Data: []byte("Dummy Line\nthis is a test file\none can test a program by running test cases"), Mode: 0755}
+    // testFS["tests/filexyz.txt"] = &fstest.MapFile{Data: []byte("no matches here"), Mode: 0755}
+    // testFS["tests/inner/test1.txt"] = &fstest.MapFile{Data: []byte("dummy file"), Mode: 0755}
+    // testFS["tests/inner/test2.txt"] = &fstest.MapFile{Data: []byte("this file contains a test line"), Mode: 0755}
 
 	testCases := []struct{
 		name string
@@ -47,12 +52,62 @@ func TestSearchString(t *testing.T) {
 		{name: "reads a file with permission error", fs: testFS, stdin: nil, fileName: file2Name, expErr: fs.ErrPermission},
 		{name: "reads an empty directory", fs: testFS, stdin: nil, fileName: dir1Name, expErr: ErrIsDirectory},
 		{name: "reads a non-existent file", fs: testFS, stdin: nil, fileName: file4Name, expErr: fs.ErrNotExist},
-		{name: "greps inside a directory with -r", fs: testFS, stdin: nil, fileName: dir2Name, keyword: "tests", ignoreCase: false, searchDir: true, expErr: nil},
+		// {name: "greps inside a directory with -r", fs: testFS, stdin: nil, fileName: dir2Name, keyword: "tests", ignoreCase: false, searchDir: true, expErr: nil},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got, err := SearchString(tc.fs, tc.fileName, bytes.NewReader(tc.stdin), tc.keyword, tc.ignoreCase)
+			got, err := SearchString(tc.fs, tc.fileName, bytes.NewReader(tc.stdin), tc.keyword, tc.ignoreCase, tc.searchDir)
+			want := tc.result
+
+			if tc.expErr != nil {
+				if err == nil {
+					t.Fatalf("Expected an error but didn't got one")
+				}
+
+				if !errors.Is(err, tc.expErr) {
+					t.Fatalf("Expected error %v but got %v", tc.expErr, err)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("Didn't expected an error: %v", err)
+			}
+
+			if !slices.Equal(got, want) {
+				t.Errorf("Expected %v but got %v", want, got)
+			}
+		})
+	}
+}
+
+func TestSearchStringR(t *testing.T) {
+	var testFS fstest.MapFS = make(map[string]*fstest.MapFile)
+	testFS["tests"] = &fstest.MapFile{Data: nil}
+	testFS["tests/test1.txt"] = &fstest.MapFile{Data: []byte("Dummy Line\nthis is a test file\none can test a program by running test cases")}
+    testFS["tests/filexyz.txt"] = &fstest.MapFile{Data: []byte("no matches here")}
+    testFS["tests/inner/test1.txt"] = &fstest.MapFile{Data: []byte("dummy file")}
+    testFS["tests/inner/test2.txt"] = &fstest.MapFile{Data: []byte("this file contains a test line")}
+
+	testCases := []struct{
+		name string
+		fs fs.FS
+		stdin []byte
+		fileName string
+		keyword string
+		ignoreCase bool
+		searchDir bool
+		result []string
+		expErr error
+	}{
+		{name: "greps inside a directory with -r", fs: testFS, stdin: nil, fileName: "tests", keyword: "tests", ignoreCase: false, result: []string{"this is a test file", "one can test a program by running test cases", "this file contains a test line"}, searchDir: true, expErr: nil},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := SearchString(tc.fs, tc.fileName, bytes.NewReader(tc.stdin), tc.keyword, tc.ignoreCase, tc.searchDir)
 			want := tc.result
 
 			if tc.expErr != nil {
